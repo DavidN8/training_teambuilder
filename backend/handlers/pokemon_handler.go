800b255874c11ebbe6e1f5741dcccf8be305057c
NahcoCZ <kponsupra@gmail.com>
2023-06-27 15:31:56 +0700

    Added placeholder handler

--- backend/handlers/pokemon_handler.go
+++ backend/handlers/pokemon_handler.go
@@ -1,3 +1,43 @@
 package handlers
 
-// TODO: CREATE HANDLER
+import (
+	"net/http"
+
+	"github.com/NahcoCZ/training_teambuilder/backend/models"
+	"github.com/NahcoCZ/training_teambuilder/backend/storage"
+	"github.com/labstack/echo/v4"
+)
+
+type PokemonHandler struct {
+	DatabaseInstance *storage.Database
+}
+
+func (ph *PokemonHandler) CreatePokemon(c echo.Context) error {
+	pokemon := &models.Pokemon{}
+
+	if err := c.Bind(pokemon); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (ph *PokemonHandler) GetPokemon(c echo.Context) error {
+	// id := c.Param("pokemonid")
+	pokemon := &models.Pokemon{}
+
+	// Get Pokemon details from the Database
+	// if err := ph.DatabaseInstance.GetPokemon(id, pokemon); err != nil {
+	// 	return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+	// }
+
+	c.JSON(http.StatusOK, echo.Map{
+		"data": pokemon,
+	})
+	return nil
+}
+
+func (ph *PokemonHandler) DeletePokemon(c echo.Context) error {
+	// id := c.Param("pokemonid")
+
+	return nil
+}
